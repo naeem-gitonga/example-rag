e9c33b2b51119225d3833f3a63be5b6a0c6b04a7
Naeem <blackb8r@gmail.com>
2026-01-26 15:23:25 -0500

    wip: added npm install script

---
+++ npm-install-everywhere.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing dependencies in shared..."
+cd shared && npm install && cd ..
+
+echo "Installing dependencies in ingestion..."
+cd ingestion && npm install && cd ..
+
+echo "Installing dependencies in query..."
+cd query && npm install && cd ..
+
+echo "All dependencies installed!"
